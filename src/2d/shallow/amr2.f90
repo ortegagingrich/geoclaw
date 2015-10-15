dabbdfe6f2823be01201276e160d4dd9232089ad
JOG-MRD <ortegagi@u.washington.edu>
2015-10-15 11:36:21 -0700

    Output hybrid Riemann solver parameters used for the run

--- src/2d/shallow/amr2.f90
+++ src/2d/shallow/amr2.f90
@@ -725,6 +725,17 @@ program amr2
     format_string="('========================== Hybrid Test Results ==========================')"
     write(*,format_string)
     write(*,*)
+    
+    !parameters used
+    print *, 'Parameters used:'
+    format_string="('roe_min_depth: ',1f5.5)"
+    write(*,format_string) roe_min_depth
+    format_string="('roe_depth_frac: ',1f5.5)"
+    write(*,format_string) roe_depth_frac
+    format_string="('roe_mom_rat: ',1f5.5)"
+    write(*,format_string) roe_mom_rat
+    
+    !solve counts
     format_string="('Total Solves: ',i15)"
     write(*,format_string) total_solves
     format_string="('Roe Solves:   ',i15)"
